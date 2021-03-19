88f175045e646a7a9fe0bc53e5d30e085f9ca7e5
sagar kumar rana <s1g1rk1m1r@gmail.com>
2021-03-19 10:54:26 +0530

    All files

---
+++ src/Home.jsx
@@ -0,0 +1,34 @@
+import './Home.css';
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+// js variables .................
+console.log("thihsfs")
+var test = <h1>this is my site</h1>;
+
+
+//  end js variables .................
+
+
+
+//   code here this is main page
+
+
+
+
+function Home() {
+  return (
+    <div className="Home">
+        <h1>thsijfshgkflkj</h1>
+      {test}
+    </div>
+  );
+}
+
+
+
+// end code of main page.........
+
+export default  Home;
+
